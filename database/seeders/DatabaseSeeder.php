2c07e8a1d77e109ae97bd635962b98f14cb79d34
David Rodriguez <davidchu1236@gmail.com>
2022-05-06 18:17:32 -0500

    Agregando todo lo relacionado a BD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+      // \App\Models\User::factory(10)->create();
+      User::factory()->create([
+        'name' => 'Italo Morales F',
+        'email' => 'i@admin.com',
+        'password' => bcrypt('123566')
+      ]);
+
+      \App\Models\Post::factory()->count(24)->create();
+
     }
 }
