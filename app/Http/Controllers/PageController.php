1c0dd9905def38cb05f4f7c5dcc80ce2b4fd07f7
David Rodriguez <davidchu1236@gmail.com>
2022-05-06 19:00:51 -0500

    Agregando diseño visual con Bootstrap

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Pagination\Paginator;
 
 class PageController extends Controller
 {
     public function posts()
     {
+      paginator::useBootstrap();
       return view('posts', [
           'posts' => Post::with('user')->latest()->paginate()
       ]);
