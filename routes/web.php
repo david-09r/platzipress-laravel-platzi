9e33896172ad0168f866a3731a0470f00e444493
David Rodriguez <davidchu1236@gmail.com>
2022-05-06 20:05:41 -0500

    Configurando vista para articulos y agregando botones

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\Backend\PostController;
+
 Route::get('/',
   [PageController::class, 'posts']
 );
@@ -24,4 +26,11 @@ Route::get('blog/{post}',
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home',
+  [App\Http\Controllers\HomeController::class, 'index']
+)->name('home');
+
+Route::resource('posts',
+  PostController::class
+)->middleware('auth')
+->except('show');
